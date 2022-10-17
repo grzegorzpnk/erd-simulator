2e2c9eb3a47d307a46ef5ee23ef4b3d3e81bbb2b
matyspi5 <piotr2.matysiak@orange.com>
2022-10-17 10:20:44 +0200

    relocate-w: minor changes in configuration & helm chart update

--- pkg/relocate-workflow/deploy.sh
+++ pkg/relocate-workflow/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-helm --kubeconfig ~/.kube/meh2.config uninstall relocate-client relocate-worker
+helm --kubeconfig ~/.kube/control.config uninstall relocate-client relocate-worker
 
 sleep 1
 
 cd deployments/helm && helm package worker/ workflowclient/ && cd ../..
 
-helm --kubeconfig ~/.kube/meh2.config install relocate-client deployments/helm/workflowclient-0.1.0.tgz
-helm --kubeconfig ~/.kube/meh2.config install relocate-worker deployments/helm/worker-0.1.0.tgz
+helm --kubeconfig ~/.kube/control.config install relocate-client deployments/helm/workflowclient-0.1.0.tgz
+helm --kubeconfig ~/.kube/control.config install relocate-worker deployments/helm/worker-0.1.0.tgz
