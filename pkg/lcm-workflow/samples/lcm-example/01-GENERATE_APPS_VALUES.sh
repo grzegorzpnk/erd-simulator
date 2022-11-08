00366bc7b44bac27809e116aadc9630a75f4a29b
matyspi5 <piotr2.matysiak@orange.com>
2022-11-08 10:24:53 +0100

    lcm-workflow: update temporal info, update sample configuration files, minor changes

--- pkg/lcm-workflow/samples/lcm-example/01-GENERATE_APPS_VALUES.sh
+++ pkg/lcm-workflow/samples/lcm-example/01-GENERATE_APPS_VALUES.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 function generateMaxLatency() {
-  echo "$(expr $((1 + $RANDOM % 20)))"
+#  echo "$(expr $((10 + $RANDOM % 30)))"
+# need different apps [16: 30ms], [17, 15ms], [17, 10ms]
+  mod=$(expr $1 % 3)
+  if [[ $mod == 0 ]]; then
+    echo 30
+  elif [[ $mod == 1 ]]; then
+      echo 15
+  elif [[ $mod == 2 ]]; then
+      echo 10
+  fi
 }
 
+#function generateMaxResUtil() {
+#   echo "$(expr $((50 + $RANDOM % 50)))"
+#}
+
 function generateMaxResUtil() {
-   echo "$(expr $((50 + $RANDOM % 50)))"
+   echo 80
 }
 
 function mebibytesToBytes() {
@@ -51,8 +64,8 @@ declare -a vars=("TARGET_CLUSTER_" "MAX_LATENCY_" "CPU_UTIL_MAX_" "MEM_UTIL_MAX_
 for app in "${apps[@]}"; do
 #    targetCluster=${clusters[$index]}
 #    index=$(expr $(($RANDOM % (${#clusters[@]}))))
-    cpu_req=$(expr $((125 + $RANDOM % 200)))
-    mem_req=$(expr $((200 + $RANDOM % 200)))
+    cpu_req=$(expr $((500 + $RANDOM % 500)))
+    mem_req=$(expr $((500 + $RANDOM % 500)))
     mem_req_bytes=$(mebibytesToBytes "$mem_req")
     cpu_req_unit="0"$(miliCpuToCpu "$cpu_req")
 
@@ -63,7 +76,7 @@ for app in "${apps[@]}"; do
       if [[ $var == "TARGET_CLUSTER_" ]]; then
         export "$temp"="\$TARGET_CLUSTER_$app"
       elif [[ $var == "MAX_LATENCY_" ]]; then
-        export "$temp"="$(generateMaxLatency)"
+        export "$temp"="$(generateMaxLatency $app)"
       elif [[ $var == "CPU_UTIL_MAX_" ]]; then
         export "$temp"="$(generateMaxResUtil)"
       elif [[ $var == "MEM_UTIL_MAX_" ]]; then
@@ -86,12 +99,17 @@ for app in "${apps[@]}"; do
   envsubst < ./emco-manifests-v2/values-template.yaml >./emco-manifests-v2/values-no-clusters.yaml
 }
 
+#declare -a apps=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10"
+#"11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30"
+#"31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "43" "44" "45" "46" "47" "48" "49" "50"
+#"51" "52" "53" "54" "55" "56" "57" "58" "59" "60" "61" "62" "63" "64" "65" "66" "67" "68" "69" "70"
+#"71" "72" "73" "74" "75" "76" "77" "78" "79" "80" "81" "82" "83" "84" "85" "86" "87" "88" "89" "90"
+#"91" "92" "93" "94" "95" "96" "97" "98" "99" "100")
+
 declare -a apps=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10"
 "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30"
 "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "43" "44" "45" "46" "47" "48" "49" "50"
-"51" "52" "53" "54" "55" "56" "57" "58" "59" "60" "61" "62" "63" "64" "65" "66" "67" "68" "69" "70"
-"71" "72" "73" "74" "75" "76" "77" "78" "79" "80" "81" "82" "83" "84" "85" "86" "87" "88" "89" "90"
-"91" "92" "93" "94" "95" "96" "97" "98" "99" "100")
+)
 
 declare -a clusters=("mec1" "mec3" "mec4" "mec5" "mec6" "mec7" "mec11" "mec12" "mec13" "mec14" "mec15" "mec16" "mec17" "mec18" "mec19" "mec20" "mec21" "mec22" "mec23" "mec24" "mec25" "mec26")
 
