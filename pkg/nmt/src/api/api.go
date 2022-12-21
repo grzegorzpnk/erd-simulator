f047c9e5b142345f25a1ef160aa84f859c487dd0
grzegorzpnk <grzegorz.panek@orange.com>
2022-12-21 18:52:56 +0100

    update apie

--- pkg/nmt/src/api/api.go
+++ pkg/nmt/src/api/api.go
@@ -25,7 +25,7 @@ func NewRouter(graphClient *mec_topology.Graph) *mux.Router {
 	r.HandleFunc(baseUrl+"/topology/cells/{cell-id}/mec-hosts", handler.getCellAssociatedMecHostsHandler).Methods("GET")
 	r.HandleFunc(baseUrl+"/topology/cells/{cell-id}/mecHosts/provider/{provider}/cluster/{cluster}/shortest-path", handler.shortestPathHandler).Methods("GET")
 	r.HandleFunc(baseUrl+"/topology/mecHosts", handler.getAllMecHostsHandler).Methods("GET")
-	r.HandleFunc(baseUrl+"/topology/mecHosts", handler.getAllMecApps).Methods("GET")
+	r.HandleFunc(baseUrl+"/topology/mecHosts/application", handler.getAllMecApps).Methods("GET")
 	r.HandleFunc(baseUrl+"/topology/mecHosts/metrics", handler.getAllMecHostsWithMetricsHandler).Methods("GET")
 	r.HandleFunc(baseUrl+"/topology/mecHosts/provider/{provider}/cluster/{cluster}", handler.getMecHostHandler).Methods("GET")
 	r.HandleFunc(baseUrl+"/topology/mecHosts/provider/{provider}/cluster/{cluster}/cpu", handler.getMECCpu).Methods("GET")
