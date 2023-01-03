a1c74fae69b3c44b9cffea1e42e83bfde62853ce
grzegorzpnk <grzegorz.panek@orange.com>
2023-01-03 16:54:20 +0100

    change in erc build script

--- pkg/erc/build.sh
+++ pkg/erc/build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-if [ -z "${ERD_REPO_NAME}" ] || [ -z "${ERD_REPO_TAG}" ]; then
-  echo "\"ERD_REPO_NAME\" and \"ERD_REPO_TAG\" environment variables must be set!"
-  exit 1
-fi
-
 make all
 
-sudo docker tag erc "$ERD_REPO_NAME"/erc:"$ERD_REPO_TAG"
+sudo docker tag erc grzegorzpnk/erc:latest
 
-sudo docker push "$ERD_REPO_NAME"/erc:"$ERD_REPO_TAG"
-
+sudo docker push grzegorzpnk/erc:latest
--- pkg/erc/deploy.sh
+++ pkg/erc/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-helm --kubeconfig "$ER_KUBECONFIG" uninstall erc
+helm --kubeconfig ~/.kube/config uninstall erc
 
 sleep 1
 
 cd deployments/helm && helm package erc/ && cd ../..
 
-helm --kubeconfig "$ER_KUBECONFIG" install erc deployments/helm/erc-0.1.0.tgz
+helm --kubeconfig ~/.kube/config install erc deployments/helm/erc-0.1.0.tgz
