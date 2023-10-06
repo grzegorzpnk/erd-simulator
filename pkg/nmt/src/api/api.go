967c244e120c7ade14850831c05c65e8348d9e9e
Grzegorz Panek <grzegorz.panek@orange.com>
2023-10-07 01:00:43 +0200

    fix

--- pkg/nmt/src/api/api.go
+++ pkg/nmt/src/api/api.go
@@ -42,8 +42,8 @@ func NewRouter(graphClient *mec_topology.Graph) *mux.Router {
 	r.HandleFunc(baseUrl+"/topology/initial-placement-generator", handler.GenerateInitialClusters).Methods("POST")
 	r.HandleFunc(baseUrl+"/topology/instantiate-all", handler.InstantiateAllDefinedApps).Methods("POST")
 	//this API defines one-click (DefineApplications & GenerateInitialClusters & InstantiateAllDefinedApps)
-	r.HandleFunc(baseUrl+"/topology/prerequesties/{applications}", handler.Prerequisites).Methods("POST")
-	r.HandleFunc(baseUrl+"/topology/prerequesties/tunning", handler.PrerequisitesTunning).Methods("POST")
+	//r.HandleFunc(baseUrl+"/topology/prerequesties/{applications}", handler.Prerequisites).Methods("POST")
+	r.HandleFunc(baseUrl+"/topology/prerequesties/max-app-tuning", handler.PrerequisitesTunning).Methods("POST")
 	r.HandleFunc(baseUrl+"/topology/recreate-initial", handler.Recreate).Methods("POST")
 
 	// interaction with ML training app (currently not used). Used only to create first config input in order to train on
