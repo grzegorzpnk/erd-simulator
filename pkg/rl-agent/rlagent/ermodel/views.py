4c192ca0332f93c035cd3c577b278f5b3cb86754
Grzegorz Panek <grzegorz.panek@orange.com>
2024-01-29 21:29:27 +0100

    fix

--- pkg/rl-agent/rlagent/ermodel/views.py
+++ pkg/rl-agent/rlagent/ermodel/views.py
@@ -39,7 +39,7 @@ def get_prediction(request: HttpRequest):
 
 #         log.info("\n\n DATA:\n", data['state'])
         log.info(f"Mask present: {use_mask}")
-        if use_mask == true
+        if use_mask == True:
             log.info(f"Mask: {mask}")
 
         cfg = config.Config(os.environ.get('CONFIG_PATH'))
